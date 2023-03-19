98b2d2b19ca8d1c762ed71060c3cc2022ea6e0d7
jangoergens <22739836+jangoergens@users.noreply.github.com>
2023-03-19 14:50:33 +0100

    Add supabase

--- apps/frontend/src/routes/+page.svelte
+++ apps/frontend/src/routes/+page.svelte
@@ -4,12 +4,8 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<span>
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</span>
-	</h1>
+	<picture>
+		<source srcset="svelte-welcome.webp" type="image/webp" />
+		<img src="svelte-welcome.png" alt="Welcome" />
+	</picture>
 </section>
