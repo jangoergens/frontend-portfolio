79c4f560519383aebf7e0cc5298766e93643c8db
jangoergens <22739836+jangoergens@users.noreply.github.com>
2023-03-19 16:10:10 +0100

    Add supabase storage of videos and comments

--- apps/frontend/src/routes/api/comments/[videoId]/+server.ts
+++ apps/frontend/src/routes/api/comments/[videoId]/+server.ts
@@ -1,58 +1,96 @@
 import { env } from '$env/dynamic/private';
+import { supabase } from '$lib/supabaseClient';
 import type { RequestEvent } from '@sveltejs/kit';
 import type { RequiredCommentInfo, YoutubeCommentThreads } from '../../../youtube/[videoId]/page';
 
 export async function GET({ params }: RequestEvent) {
 	if (env.GOOGLE_API_MODE === 'production') {
-		const apiKey = env.GOOGLE_API_KEY;
-		const MAX_RESULTS = 20;
-		const MAX_COMMENT_THREAD_PAGES = 10;
-
-		const fetchCommentThread = async (pageToken?: string) => {
-			return await fetch(
-				`https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&order=relevance&videoId=${
-					params.videoId
-				}&key=${apiKey}&textFormat=plainText&maxResults=100${
-					pageToken ? `&pageToken=${pageToken}` : ''
-				}`
-			);
-		};
-
-		let commentThreads: YoutubeCommentThreads | undefined;
-		let currentCommentThreadPage = 0;
-		const topComments: RequiredCommentInfo[] = [];
-
-		do {
-			const response = await fetchCommentThread(commentThreads?.nextPageToken);
-			currentCommentThreadPage++;
-
-			if (response.ok) {
-				commentThreads = await response.json();
-				commentThreads?.items.map((commentThread) => {
-					const topComment: RequiredCommentInfo = {
-						textDisplay: commentThread.snippet.topLevelComment.snippet.textDisplay,
-						authorDisplayName: commentThread.snippet.topLevelComment.snippet.authorDisplayName,
-						authorProfileImageUrl:
-							commentThread.snippet.topLevelComment.snippet.authorProfileImageUrl,
-						authorChannelUrl: commentThread.snippet.topLevelComment.snippet.authorChannelUrl,
-						likeCount: commentThread.snippet.topLevelComment.snippet.likeCount,
-						publishedAt: commentThread.snippet.topLevelComment.snippet.publishedAt
-					};
-					if (topComments.length < MAX_RESULTS) {
-						topComments.push(topComment);
-						topComments.sort((a, b) => Number(b.likeCount) - Number(a.likeCount));
-					} else if (topComment.likeCount > topComments[MAX_RESULTS - 1].likeCount) {
-						topComments.pop();
-						topComments.push(topComment);
-						topComments.sort((a, b) => Number(b.likeCount) - Number(a.likeCount));
-					}
+		const { data } = await supabase.from('videos').select().eq('video_id', params.videoId);
+		if (data && data[0]) {
+			const { data } = await supabase.from('comments').select().eq('video_id', params.videoId);
+			const topComments: RequiredCommentInfo[] = [];
+			data?.map((comment) => {
+				topComments.push({
+					textDisplay: comment.text_display,
+					authorDisplayName: comment.author_display_name,
+					authorProfileImageUrl: comment.author_profile_image_url,
+					authorChannelUrl: comment.author_channel_url,
+					likeCount: comment.like_count,
+					publishedAt: comment.published_at
 				});
-			} else {
-				return new Response(JSON.stringify(response.text()));
-			}
-		} while (commentThreads?.nextPageToken && currentCommentThreadPage < MAX_COMMENT_THREAD_PAGES);
+			});
+			return new Response(
+				JSON.stringify(topComments.sort((a, b) => Number(b.likeCount) - Number(a.likeCount)))
+			);
+		} else {
+			await supabase.from('videos').insert({ video_id: params.videoId });
+			const apiKey = env.GOOGLE_API_KEY;
+			const MAX_RESULTS = 20;
+			const MAX_COMMENT_THREAD_PAGES = 10;
 
-		return new Response(JSON.stringify(topComments));
+			const fetchCommentThread = async (pageToken?: string) => {
+				return await fetch(
+					`https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&order=relevance&videoId=${
+						params.videoId
+					}&key=${apiKey}&textFormat=plainText&maxResults=100${
+						pageToken ? `&pageToken=${pageToken}` : ''
+					}`
+				);
+			};
+
+			let commentThreads: YoutubeCommentThreads | undefined;
+			let currentCommentThreadPage = 0;
+			const topComments: RequiredCommentInfo[] = [];
+
+			do {
+				const response = await fetchCommentThread(commentThreads?.nextPageToken);
+				currentCommentThreadPage++;
+
+				if (response.ok) {
+					commentThreads = await response.json();
+					commentThreads?.items.map((commentThread) => {
+						const topComment: RequiredCommentInfo = {
+							textDisplay: commentThread.snippet.topLevelComment.snippet.textDisplay,
+							authorDisplayName: commentThread.snippet.topLevelComment.snippet.authorDisplayName,
+							authorProfileImageUrl:
+								commentThread.snippet.topLevelComment.snippet.authorProfileImageUrl,
+							authorChannelUrl: commentThread.snippet.topLevelComment.snippet.authorChannelUrl,
+							likeCount: commentThread.snippet.topLevelComment.snippet.likeCount,
+							publishedAt: commentThread.snippet.topLevelComment.snippet.publishedAt
+						};
+						if (topComments.length < MAX_RESULTS) {
+							topComments.push(topComment);
+							topComments.sort((a, b) => Number(b.likeCount) - Number(a.likeCount));
+						} else if (topComment.likeCount > topComments[MAX_RESULTS - 1].likeCount) {
+							topComments.pop();
+							topComments.push(topComment);
+							topComments.sort((a, b) => Number(b.likeCount) - Number(a.likeCount));
+						}
+					});
+				} else {
+					return new Response(JSON.stringify(response.text()));
+				}
+			} while (
+				commentThreads?.nextPageToken &&
+				currentCommentThreadPage < MAX_COMMENT_THREAD_PAGES
+			);
+
+			await supabase.from('comments').insert(
+				topComments.map((comment) => {
+					return {
+						video_id: params.videoId,
+						text_display: comment.textDisplay,
+						author_display_name: comment.authorDisplayName,
+						author_profile_image_url: comment.authorProfileImageUrl,
+						author_channel_url: comment.authorChannelUrl,
+						like_count: comment.likeCount,
+						published_at: comment.publishedAt
+					};
+				})
+			);
+
+			return new Response(JSON.stringify(topComments));
+		}
 	} else if (env.GOOGLE_API_MODE === 'development') {
 		const randomComments: RequiredCommentInfo[] = [
 			{
