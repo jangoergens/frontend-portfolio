7ec3691ab32ee19a563bd1dba31391cf7546defc
jangoergens <22739836+jangoergens@users.noreply.github.com>
2023-03-25 20:41:55 +0100

    Improve homepage design

--- apps/frontend/src/routes/+page.svelte
+++ apps/frontend/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import searchIcon from '$lib/assets/searchWhite.svg';
 
 	let videoURL = '';
 
@@ -23,29 +24,41 @@
 	/>
 </svelte:head>
 
-<section class="flex flex-col justify-center items-center gap-12">
-	<div class="flex flex-col items-center">
-		<h1>TopCommentFinder - Beyond the Algorithm</h1>
-		<h2>Access top comments without YouTube's limitations.</h2>
-	</div>
-	<p>
-		The existing YouTube comment algorithm may not cater to everyone's preferences, especially when
-		browsing older videos. To address this, we've developed a user-friendly platform that enables
-		you to effortlessly access the genuine top comments from any YouTube video. All you need to do
-		is enter the video's link.
-	</p>
-	<form on:submit|preventDefault={handleSubmit}>
-		<label for="videoSearch">Video URL: </label>
+<section class="flex flex-col justify-center items-center">
+	<h1 class="font-extrabold text-2xl sm:text-3xl lg:text-4xl tracking-tight text-center">
+		TopCommentFinder - Beyond the Algorithm
+	</h1>
+	<h2 class="text-center italic text-slate-500">
+		Access top comments without YouTube's limitations
+	</h2>
+	<form
+		on:submit|preventDefault={handleSubmit}
+		class="my-8 w-full sm:w-2/3 lg:max-w-xl py-8 flex gap-2 items-center"
+	>
 		<input
-			class="border-2 border-black rounded"
+			class="border-2 border-black rounded-full h-10 px-2 w-5/6 text-center"
 			type="text"
 			name="videoSearch"
 			id="videoSearch"
+			placeholder="Paste YouTube Video URL"
 			bind:value={videoURL}
 		/>
-		<button type="submit">Search</button>
+		<button type="submit" title="Search" class="h-9 py-1"
+			><img src={searchIcon} alt="Search Icon" class="h-full fill-white" /></button
+		>
 	</form>
 
-	<a href="/Y4iXIdg8b10"><button type="button">Example Video (few comments)</button></a>
-	<a href="/2L2wEs2Cbdk"><button type="button">Example Video (many comments)</button></a>
+	<div class="flex flex-col sm:w-2/3 md:w-1/2 xl:w-2/5  gap-4 text-center font-semibold ">
+		<p>
+			The existing YouTube comment-sorting algorithm may not cater to everyone's preferences,
+			especially when browsing older videos.
+		</p>
+		<p>
+			To address this, we've developed a user-friendly platform that enables you to effortlessly
+			access the genuine top comments from any YouTube video. All you need to do is enter the
+			video's link.
+		</p>
+	</div>
+
+	<a href="/Y4iXIdg8b10" class="my-8"><button type="button">Give it a try!</button></a>
 </section>
