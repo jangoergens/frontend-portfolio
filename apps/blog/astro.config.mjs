f83d2b593f4fcdf8cf7384962c13ebc576a3dde6
jangoergens <22739836+jangoergens@users.noreply.github.com>
2023-10-15 19:28:15 +0200

    Add tailwind to blog

--- apps/blog/astro.config.mjs
+++ apps/blog/astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
-	// Enable Svelte to support Svelte components.
-	integrations: [svelte()],
+	integrations: [svelte(), tailwind()],
 });
