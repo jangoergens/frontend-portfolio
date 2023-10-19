6ccfeb7021aad864e5405fdff6ba7f64fb00d7e9
jangoergens <22739836+jangoergens@users.noreply.github.com>
2023-10-19 19:32:37 +0200

    Add astro ts plugin

--- apps/blog/src/env.d.ts
+++ apps/blog/src/env.d.ts
@@ -1,4 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+/// <reference types="vite/client" />
 
 import 'astro/astro-jsx';
 
