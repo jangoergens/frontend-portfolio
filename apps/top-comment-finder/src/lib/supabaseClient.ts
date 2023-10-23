1703c4f3c96e2c642da7661d0dbfcecb66e4fd2b
jangoergens <22739836+jangoergens@users.noreply.github.com>
2023-10-24 00:42:51 +0200

    Allow building of tcf without credentials

--- apps/top-comment-finder/src/lib/supabaseClient.ts
+++ apps/top-comment-finder/src/lib/supabaseClient.ts
@@ -1,6 +1,14 @@
 import type { Database } from '$lib/types/supabase';
+import type { SupabaseClient } from '@supabase/supabase-js';
 
+import { env } from '$env/dynamic/private';
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
 import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient<Database>(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
+let supabase: SupabaseClient<Database> | undefined;
+
+if (env.GOOGLE_API_MODE === 'production') {
+	supabase = createClient<Database>(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
+}
+
+export { supabase };
